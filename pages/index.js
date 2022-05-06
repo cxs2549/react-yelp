305b8613c3c8f17bcb1e764174fcd3ba4d577986
cxs2549 <sanchez.cliff.86@gmail.com>
2022-05-06 16:48:44 -0700

    dev start

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const StyledSearch = styled.div`
+  background-color: lightgreen;
+  height: 100vh;
+  overflow-y: scroll;
+`
+
+const Search = () => {
+  return <StyledSearch>yelp!</StyledSearch>
+}
+export default Search
